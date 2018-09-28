40ff0a5ac2dbc6de51e74c4ef74b95e243b4e1e3
makarabbit <39292276+makarabbit@users.noreply.github.com>
2018-09-28 11:54:32 +0900

    site2up

--- design2/main.js
+++ design2/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var wd = parseInt($("#con1").width())*-1;
     var intv = setInterval(function(){ vAni() }, 4000);
     function vAni(){
-        $("#con1 .imgBox").not(":animated").animate({ "margin-left" : wd+"px"}, 800, function(){
+        $("#con1 .imgBox").not(":animated").animate({ "margin-left" : wd+"px"}, 1200, function(){
             $("#con1 .imgBox li").eq(0).appendTo($("#con1 .imgBox"));
             $("#con1 .imgBox").css("margin-left", "0px");
         });
@@ -10,7 +10,7 @@ $(document).ready(function(){
     
     var g_wd = parseInt($("#vs1").width())*-1;
     $("#vs1 .more").click(function(){
-       $(".imgBox2").not(":animated").animate({"margin-left" : g_wd+"px"}, 800, function(){
+       $(".imgBox2").not(":animated").animate({"margin-left" : g_wd+"px"}, 1200, function(){
           $(".imgBox2 li").eq(0).appendTo($(".imgBox2"));
            $(".imgBox2").css("margin-left", "0px");
        }) 
